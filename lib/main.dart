68fd674e365f17bebc5c54ad24c1942617561f7e
Mamaulana7 <maztagbals@gmail.com>
2022-08-30 09:41:45 +0700

    tambah title

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ini saya',
+      title: 'kamu',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'ini saya Home Page'),
+      home: const MyHomePage(title: 'kamu Home Page'),
     );
   }
 }
