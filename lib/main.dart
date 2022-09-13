96f4a4058027daf7a93b31330b98313e8f4f5fc6
cature04 <catursubandiono10@gmail.com>
2022-09-12 19:37:07 -0700

    konn

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ini saya',
+      title: 'ini ',
       theme: ThemeData(
         // This is the theme of your application.
         //
